c327b66b78637ea885f93a3545ca3e8e9a002269
Todd Poynor <toddpoynor@google.com>
2011-08-24 15:01:30 -0700

    ANDROID: fs: fuse: Freeze client on suspend when request sent to userspace
    Suspend attempts can abort when the FUSE daemon is already frozen
    and a client is waiting uninterruptibly for a response, causing
    freezing of tasks to fail.
    
    Use the freeze-friendly wait API, but disregard other signals.
    
    Change-Id: Icefb7e4bbc718ccb76bf3c04daaa5eeea7e0e63c
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -22,6 +22,7 @@
 #include <linux/swap.h>
 #include <linux/splice.h>
 #include <linux/sched.h>
+#include <linux/freezer.h>
 #include <linux/sched/mm.h>
 
 MODULE_ALIAS_MISCDEV(FUSE_MINOR);
@@ -497,7 +498,9 @@ static void request_wait_answer(struct fuse_conn *fc, struct fuse_req *req)
 	 * Either request is already in userspace, or it was forced.
 	 * Wait it out.
 	 */
-	fuse_wait_event(req->waitq, test_bit(FR_FINISHED, &req->flags));
+	while (!test_bit(FR_FINISHED, &req->flags))
+		fuse_wait_event(req->waitq,
+				test_bit(FR_FINISHED, &req->flags));
 }
 
 static void __fuse_request_send(struct fuse_conn *fc, struct fuse_req *req)
