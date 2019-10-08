41260e12804228d4ba5be5f18d29b0197bb52a8d
Gao Xiang <gaoxiang25@huawei.com>
2019-10-08 20:56:16 +0800

    erofs: set iowait for sync decompression
    For those tasks waiting I/O for sync decompression,
    they should be better marked as IO wait state.
    
    Link: https://lore.kernel.org/r/20191008125616.183715-5-gaoxiang25@huawei.com
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Change-Id: I6922f7e3201b5d69642167c882f5580fb4d11a03
    Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -1524,7 +1524,8 @@ static void z_erofs_runqueue(struct super_block *sb,
 		return;
 
 	/* wait until all bios are completed */
-	wait_for_completion_io(&io[JQ_SUBMIT].u.done);
+	io_wait_event(io[JQ_SUBMIT].u.wait,
+		      !atomic_read(&io[JQ_SUBMIT].pending_bios));
 
 	/* let's synchronous decompression */
 	z_erofs_vle_unzip_all(&io[JQ_SUBMIT], pagepool);
