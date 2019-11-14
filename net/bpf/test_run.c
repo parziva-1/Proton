93bddce40000f0fc35470844e197845ea785d2b3
Alexei Starovoitov <ast@kernel.org>
2019-11-14 10:57:08 -0800

    UPSTREAM: bpf: Add kernel test functions for fentry testing
    Add few kernel functions with various number of arguments,
    their types and sizes for BPF trampoline testing to cover
    different calling conventions.
    
    Change-Id: I8b0a9abb30cf4962db2617f008311769f97590ea
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20191114185720.1641606-9-ast@kernel.org

--- net/bpf/test_run.c
+++ net/bpf/test_run.c
@@ -160,9 +160,6 @@ out:
  * architecture dependent calling conventions. 7+ can be supported in the
  * future.
  */
-__diag_push();
-__diag_ignore(GCC, 8, "-Wmissing-prototypes",
-	      "Global functions as their definitions will be in vmlinux BTF");
 int noinline bpf_fentry_test1(int a)
 {
 	return a + 1;
@@ -193,29 +190,6 @@ int noinline bpf_fentry_test6(u64 a, void *b, short c, int d, void *e, u64 f)
 	return a + (long)b + c + d + (long)e + f;
 }
 
-struct bpf_fentry_test_t {
-	struct bpf_fentry_test_t *a;
-};
-
-int noinline bpf_fentry_test7(struct bpf_fentry_test_t *arg)
-{
-	return (long)arg;
-}
-
-int noinline bpf_fentry_test8(struct bpf_fentry_test_t *arg)
-{
-	return (long)arg->a;
-}
-
-int noinline bpf_modify_return_test(int a, int *b)
-{
-	*b += 1;
-	return a + *b;
-}
-__diag_pop();
-
-ALLOW_ERROR_INJECTION(bpf_modify_return_test, ERRNO);
-
 static void *bpf_test_init(const union bpf_attr *kattr, u32 size,
 			   u32 headroom, u32 tailroom)
 {
@@ -238,7 +212,13 @@ static void *bpf_test_init(const union bpf_attr *kattr, u32 size,
 		kfree(data);
 		return ERR_PTR(-EFAULT);
 	}
-
+	if (bpf_fentry_test1(1) != 2 ||
+	    bpf_fentry_test2(2, 3) != 5 ||
+	    bpf_fentry_test3(4, 5, 6) != 15 ||
+	    bpf_fentry_test4((void *)7, 8, 9, 10) != 34 ||
+	    bpf_fentry_test5(11, (void *)12, 13, 14, 15) != 65 ||
+	    bpf_fentry_test6(16, (void *)17, 18, 19, (void *)20, 21) != 111)
+		return ERR_PTR(-EFAULT);
 	return data;
 }
 
