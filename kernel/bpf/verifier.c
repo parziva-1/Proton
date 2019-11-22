3b179b297086829ffae724c887d51e58d4f02bfd
Daniel Borkmann <daniel@iogearbox.net>
2019-11-22 21:07:59 +0100

    UPSTREAM: bpf: Constant map key tracking for prog array pokes
    Add tracking of constant keys into tail call maps. The signature of
    bpf_tail_call_proto is that arg1 is ctx, arg2 map pointer and arg3
    is a index key. The direct call approach for tail calls can be enabled
    if the verifier asserted that for all branches leading to the tail call
    helper invocation, the map pointer and index key were both constant
    and the same.
    
    Tracking of map pointers we already do from prior work via c93552c443eb
    ("bpf: properly enforce index mask to prevent out-of-bounds speculation")
    and 09772d92cd5a ("bpf: avoid retpoline for lookup/update/ delete calls
    on maps").
    
    Given the tail call map index key is not on stack but directly in the
    register, we can add similar tracking approach and later in fixup_bpf_calls()
    add a poke descriptor to the progs poke_tab with the relevant information
    for the JITing phase.
    
    We internally reuse insn->imm for the rewritten BPF_JMP | BPF_TAIL_CALL
    instruction in order to point into the prog's poke_tab, and keep insn->imm
    as 0 as indicator that current indirect tail call emission must be used.
    Note that publishing to the tracker must happen at the end of fixup_bpf_calls()
    since adding elements to the poke_tab reallocates its memory, so we need
    to wait until its in final state.
    
    Future work can generalize and add similar approach to optimize plain
    array map lookups. Difference there is that we need to look into the key
    value that sits on stack. For clarity in bpf_insn_aux_data, map_state
    has been renamed into map_ptr_state, so we get map_{ptr,key}_state as
    trackers.
    
    Change-Id: I5cae67ff5b2e9c12aad45f8329a1abd8026ed005
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/e8db37f6b2ae60402fa40216c96738ee9b316c32.1574452833.git.daniel@iogearbox.net

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -5376,7 +5376,7 @@ record_func_map(struct bpf_verifier_env *env, struct bpf_call_arg_meta *meta,
 
 	if (!BPF_MAP_PTR(aux->map_ptr_state))
 		bpf_map_ptr_store(aux, meta->map_ptr,
-				  !meta->map_ptr->bypass_spec_v1);
+				  meta->map_ptr->unpriv_array);
 	else if (BPF_MAP_PTR(aux->map_ptr_state) != meta->map_ptr)
 		bpf_map_ptr_store(aux, BPF_MAP_PTR_POISON,
 				  !meta->map_ptr->bypass_spec_v1);
@@ -5412,6 +5412,40 @@ record_func_key(struct bpf_verifier_env *env, struct bpf_call_arg_meta *meta,
 	err = mark_chain_precision(env, BPF_REG_3);
 	if (err)
 		return err;
+	if (bpf_map_key_unseen(aux))
+		bpf_map_key_store(aux, val);
+	else if (!bpf_map_key_poisoned(aux) &&
+		  bpf_map_key_immediate(aux) != val)
+		bpf_map_key_store(aux, BPF_MAP_KEY_POISON);
+	return 0;
+}
+
+static int
+record_func_key(struct bpf_verifier_env *env, struct bpf_call_arg_meta *meta,
+		int func_id, int insn_idx)
+{
+	struct bpf_insn_aux_data *aux = &env->insn_aux_data[insn_idx];
+	struct bpf_reg_state *regs = cur_regs(env), *reg;
+	struct bpf_map *map = meta->map_ptr;
+	struct tnum range;
+	u64 val;
+
+	if (func_id != BPF_FUNC_tail_call)
+		return 0;
+	if (!map || map->map_type != BPF_MAP_TYPE_PROG_ARRAY) {
+		verbose(env, "kernel subsystem misconfigured verifier\n");
+		return -EINVAL;
+	}
+
+	range = tnum_range(0, map->max_entries - 1);
+	reg = &regs[BPF_REG_3];
+
+	if (!register_is_const(reg) || !tnum_in(range, reg->var_off)) {
+		bpf_map_key_store(aux, BPF_MAP_KEY_POISON);
+		return 0;
+	}
+
+	val = reg->var_off.value;
 	if (bpf_map_key_unseen(aux))
 		bpf_map_key_store(aux, val);
 	else if (!bpf_map_key_poisoned(aux) &&
@@ -5494,6 +5528,10 @@ static int check_helper_call(struct bpf_verifier_env *env, int func_id, int insn
 		if (err)
 			return err;
 	}
+
+	err = record_func_key(env, &meta, func_id, insn_idx);
+	if (err)
+		return err;
 
 	err = record_func_key(env, &meta, func_id, insn_idx);
 	if (err)
@@ -11711,8 +11749,7 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 			insn->code = BPF_JMP | BPF_TAIL_CALL;
 
 			aux = &env->insn_aux_data[i + delta];
-			if (env->bpf_capable && !expect_blinding &&
-			    prog->jit_requested &&
+			if (prog->jit_requested && !expect_blinding &&
 			    !bpf_map_key_poisoned(aux) &&
 			    !bpf_map_ptr_poisoned(aux) &&
 			    !bpf_map_ptr_unpriv(aux)) {
@@ -11720,7 +11757,6 @@ static int fixup_bpf_calls(struct bpf_verifier_env *env)
 					.reason = BPF_POKE_REASON_TAIL_CALL,
 					.tail_call.map = BPF_MAP_PTR(aux->map_ptr_state),
 					.tail_call.key = bpf_map_key_immediate(aux),
-					.insn_idx = i + delta,
 				};
 
 				ret = bpf_jit_add_poke_descriptor(prog, &desc);
