2c91772ff038432a9a74c28a6a4acfc6f2fe59a6
Alexei Starovoitov <ast@kernel.org>
2020-01-31 16:03:14 -0800

    UPSTREAM: bpf: Fix modifier skipping logic
    Fix the way modifiers are skipped while walking pointers. Otherwise second
    level dereferences of 'const struct foo *' will be rejected by the verifier.
    
    Fixes: 9e15db66136a ("bpf: Implement accurate raw_tp context access via BTF")
    Change-Id: I944e3a79943b8b7bccf3e4a5b6652b6b7034abee
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20200201000314.261392-1-ast@kernel.org

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -4552,6 +4552,7 @@ again:
 
 		if (btf_type_is_ptr(mtype)) {
 			const struct btf_type *stype;
+			u32 id;
 
 			if (msize != size || off != moff) {
 				bpf_log(log,
@@ -4560,12 +4561,9 @@ again:
 				return -EACCES;
 			}
 
-			stype = btf_type_by_id(btf_vmlinux, mtype->type);
-			/* skip modifiers */
-			while (btf_type_is_modifier(stype))
-				stype = btf_type_by_id(btf_vmlinux, stype->type);
+			stype = btf_type_skip_modifiers(btf_vmlinux, mtype->type, &id);
 			if (btf_type_is_struct(stype)) {
-				*next_btf_id = mtype->type;
+				*next_btf_id = id;
 				return PTR_TO_BTF_ID;
 			}
 		}
