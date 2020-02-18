f81353068e8f17388f71f58c3da30e28aaa9900d
Daniel Xu <dxu@dxuuu.xyz>
2020-02-17 19:04:31 -0800

    BACKPORT: bpf: Add bpf_read_branch_records() helper
    Branch records are a CPU feature that can be configured to record
    certain branches that are taken during code execution. This data is
    particularly interesting for profile guided optimizations. perf has had
    branch record support for a while but the data collection can be a bit
    coarse grained.
    
    We (Facebook) have seen in experiments that associating metadata with
    branch records can improve results (after postprocessing). We generally
    use bpf_probe_read_*() to get metadata out of userspace. That's why bpf
    support for branch records is useful.
    
    Aside from this particular use case, having branch data available to bpf
    progs can be useful to get stack traces out of userspace applications
    that omit frame pointers.
    
    Change-Id: I6475b600df60571d1b0daa985eff1ee01c8886eb
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200218030432.4600-2-dxu@dxuuu.xyz

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -3036,6 +3036,25 @@ union bpf_attr {
  *		Obtain the 64bit jiffies
  *	Return
  *		The 64 bit jiffies
+ *
+ * int bpf_read_branch_records(struct bpf_perf_event_data *ctx, void *buf, u32 size, u64 flags)
+ *	Description
+ *		For an eBPF program attached to a perf event, retrieve the
+ *		branch records (struct perf_branch_entry) associated to *ctx*
+ *		and store it in	the buffer pointed by *buf* up to size
+ *		*size* bytes.
+ *	Return
+ *		On success, number of bytes written to *buf*. On error, a
+ *		negative value.
+ *
+ *		The *flags* can be set to **BPF_F_GET_BRANCH_RECORDS_SIZE** to
+ *		instead	return the number of bytes required to store all the
+ *		branch entries. If this flag is set, *buf* may be NULL.
+ *
+ *		**-EINVAL** if arguments invalid or **size** not a multiple
+ *		of sizeof(struct perf_branch_entry).
+ *
+ *		**-ENOENT** if architecture does not support branch records.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -3346,6 +3365,9 @@ enum {
 	BPF_SK_LOOKUP_F_REPLACE		= (1ULL << 0),
 	BPF_SK_LOOKUP_F_NO_REUSEPORT	= (1ULL << 1),
 };
+
+/* BPF_FUNC_read_branch_records flags. */
+#define BPF_F_GET_BRANCH_RECORDS_SIZE	(1ULL << 0)
 
 /* Mode for BPF_FUNC_skb_adjust_room helper. */
 enum bpf_adj_room_mode {
