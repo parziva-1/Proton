f0ab5600f97a07cf8f50a672176f8cf3f4d4dd60
Björn Töpel <bjorn.topel@intel.com>
2020-03-12 20:55:57 +0100

    BACKPORT: bpf: Add bpf_trampoline_ name prefix for DECLARE_BPF_DISPATCHER
    Adding bpf_trampoline_ name prefix for DECLARE_BPF_DISPATCHER,
    so all the dispatchers have the common name prefix.
    
    And also a small '_' cleanup for bpf_dispatcher_nopfunc function
    name.
    
    Change-Id: Ib51f0c0a0eef755dc9779d733e3e17be5936daf3
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200312195610.346362-3-jolsa@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -10447,10 +10447,9 @@ const struct bpf_verifier_ops sk_lookup_verifier_ops = {
 
 #endif /* CONFIG_INET */
 
-DEFINE_BPF_DISPATCHER(bpf_dispatcher_xdp)
+DEFINE_BPF_DISPATCHER(xdp)
 
 void bpf_prog_change_xdp(struct bpf_prog *prev_prog, struct bpf_prog *prog)
 {
-	bpf_dispatcher_change_prog(BPF_DISPATCHER_PTR(bpf_dispatcher_xdp),
-				   prev_prog, prog);
+	bpf_dispatcher_change_prog(BPF_DISPATCHER_PTR(xdp), prev_prog, prog);
 }
