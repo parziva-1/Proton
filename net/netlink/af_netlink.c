bfba62330833726127f8ce94ec207b4fa7840a02
Yonghong Song <yhs@fb.com>
2020-05-09 10:59:10 -0700

    BACKPORT: net: bpf: Add netlink and ipv6_route bpf_iter targets
    This patch added netlink and ipv6_route targets, using
    the same seq_ops (except show() and minor changes for stop())
    for /proc/net/{netlink,ipv6_route}.
    
    The net namespace for these targets are the current net
    namespace at file open stage, similar to
    /proc/net/{netlink,ipv6_route} reference counting
    the net namespace at seq_file open stage.
    
    Since module is not supported for now, ipv6_route is
    supported only if the IPV6 is built-in, i.e., not compiled
    as a module. The restriction can be lifted once module
    is properly supported for bpf_iter.
    
    Change-Id: I07ea2bf9ae25cbca90fd56b70ef6b7bf76fe996d
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200509175910.2476329-1-yhs@fb.com

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -2821,30 +2821,17 @@ static const struct rhashtable_params netlink_rhashtable_params = {
 };
 
 #if defined(CONFIG_BPF_SYSCALL) && defined(CONFIG_PROC_FS)
-BTF_ID_LIST(btf_netlink_sock_id)
-BTF_ID(struct, netlink_sock)
-
-static const struct bpf_iter_seq_info netlink_seq_info = {
-	.seq_ops		= &netlink_seq_ops,
-	.init_seq_private	= bpf_iter_init_seq_net,
-	.fini_seq_private	= bpf_iter_fini_seq_net,
-	.seq_priv_size		= sizeof(struct nl_seq_iter),
-};
-
-static struct bpf_iter_reg netlink_reg_info = {
-	.target			= "netlink",
-	.ctx_arg_info_size	= 1,
-	.ctx_arg_info		= {
-		{ offsetof(struct bpf_iter__netlink, sk),
-		  PTR_TO_BTF_ID_OR_NULL },
-	},
-	.seq_info		= &netlink_seq_info,
-};
-
 static int __init bpf_iter_register(void)
 {
-	netlink_reg_info.ctx_arg_info[0].btf_id = *btf_netlink_sock_id;
-	return bpf_iter_reg_target(&netlink_reg_info);
+	struct bpf_iter_reg reg_info = {
+		.target			= "netlink",
+		.seq_ops		= &netlink_seq_ops,
+		.init_seq_private	= bpf_iter_init_seq_net,
+		.fini_seq_private	= bpf_iter_fini_seq_net,
+		.seq_priv_size		= sizeof(struct nl_seq_iter),
+	};
+
+	return bpf_iter_reg_target(&reg_info);
 }
 #endif
 
