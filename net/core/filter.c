dda1b6784bda98059209e267509d8de170af64d4
Andrey Ignatov <rdna@fb.com>
2020-05-14 13:03:46 -0700

    UPSTREAM: bpf: Allow skb_ancestor_cgroup_id helper in cgroup skb
    cgroup skb programs already can use bpf_skb_cgroup_id. Allow
    bpf_skb_ancestor_cgroup_id as well so that container policies can be
    implemented for a container that can have sub-cgroups dynamically
    created, but policies should still be implemented based on cgroup id of
    container itself not on an id of a sub-cgroup.
    
    Change-Id: Ib16692dd042b2c5062c75c74c776ac629daa63dc
    Signed-off-by: Andrey Ignatov <rdna@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/8874194d6041eba190356453ea9f6071edf5f658.1589486450.git.rdna@fb.com

--- net/core/filter.c
+++ net/core/filter.c
@@ -6807,10 +6807,6 @@ cg_skb_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 		return &bpf_skb_cgroup_id_proto;
 	case BPF_FUNC_skb_ancestor_cgroup_id:
 		return &bpf_skb_ancestor_cgroup_id_proto;
-	case BPF_FUNC_sk_cgroup_id:
-		return &bpf_sk_cgroup_id_proto;
-	case BPF_FUNC_sk_ancestor_cgroup_id:
-		return &bpf_sk_ancestor_cgroup_id_proto;
 #endif
 #ifdef CONFIG_INET
 	case BPF_FUNC_sk_lookup_tcp:
