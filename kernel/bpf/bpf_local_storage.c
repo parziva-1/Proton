9531619c96e88fb8f9b4a9b6f693e11c8aacc53a
KP Singh <kpsingh@google.com>
2020-08-25 20:29:16 +0200

    UPSTREAM: bpf: Split bpf_local_storage to bpf_sk_storage
    A purely mechanical change:
    
    	bpf_sk_storage.c = bpf_sk_storage.c + bpf_local_storage.c
    	bpf_sk_storage.h = bpf_sk_storage.h + bpf_local_storage.h
    
    Change-Id: Ie2a4690a8943b94a891dd2e34e47a0d4d9af692c
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200825182919.1118197-5-kpsingh@chromium.org

--- kernel/bpf/bpf_local_storage.c
+++ kernel/bpf/bpf_local_storage.c
@@ -48,19 +48,9 @@ owner_storage(struct bpf_local_storage_map *smap, void *owner)
 	return map->ops->map_owner_storage_ptr(owner);
 }
 
-static bool selem_linked_to_storage_lockless(const struct bpf_local_storage_elem *selem)
-{
-	return !hlist_unhashed_lockless(&selem->snode);
-}
-
 static bool selem_linked_to_storage(const struct bpf_local_storage_elem *selem)
 {
 	return !hlist_unhashed(&selem->snode);
-}
-
-static bool selem_linked_to_map_lockless(const struct bpf_local_storage_elem *selem)
-{
-	return !hlist_unhashed_lockless(&selem->map_node);
 }
 
 static bool selem_linked_to_map(const struct bpf_local_storage_elem *selem)
@@ -80,7 +70,7 @@ bpf_selem_alloc(struct bpf_local_storage_map *smap, void *owner,
 	selem = kzalloc(smap->elem_size, GFP_ATOMIC | __GFP_NOWARN);
 	if (selem) {
 		if (value)
-			copy_map_value(&smap->map, SDATA(selem)->data, value);
+			memcpy(SDATA(selem)->data, value, smap->map.value_size);
 		return selem;
 	}
 
@@ -150,7 +140,7 @@ static void __bpf_selem_unlink_storage(struct bpf_local_storage_elem *selem)
 	struct bpf_local_storage *local_storage;
 	bool free_local_storage = false;
 
-	if (unlikely(!selem_linked_to_storage_lockless(selem)))
+	if (unlikely(!selem_linked_to_storage(selem)))
 		/* selem has already been unlinked from sk */
 		return;
 
@@ -169,7 +159,7 @@ void bpf_selem_link_storage_nolock(struct bpf_local_storage *local_storage,
 				   struct bpf_local_storage_elem *selem)
 {
 	RCU_INIT_POINTER(selem->local_storage, local_storage);
-	hlist_add_head_rcu(&selem->snode, &local_storage->list);
+	hlist_add_head(&selem->snode, &local_storage->list);
 }
 
 void bpf_selem_unlink_map(struct bpf_local_storage_elem *selem)
@@ -177,7 +167,7 @@ void bpf_selem_unlink_map(struct bpf_local_storage_elem *selem)
 	struct bpf_local_storage_map *smap;
 	struct bpf_local_storage_map_bucket *b;
 
-	if (unlikely(!selem_linked_to_map_lockless(selem)))
+	if (unlikely(!selem_linked_to_map(selem)))
 		/* selem has already be unlinked from smap */
 		return;
 
@@ -375,7 +365,7 @@ bpf_local_storage_update(void *owner, struct bpf_local_storage_map *smap,
 		err = check_flags(old_sdata, map_flags);
 		if (err)
 			return ERR_PTR(err);
-		if (old_sdata && selem_linked_to_storage_lockless(SELEM(old_sdata))) {
+		if (old_sdata && selem_linked_to_storage(SELEM(old_sdata))) {
 			copy_map_value_locked(&smap->map, old_sdata->data,
 					      value, false);
 			return old_sdata;
