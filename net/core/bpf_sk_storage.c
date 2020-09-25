b239628ffa22fd1ba9fd2796d1061c9679787254
Martin KaFai Lau <kafai@fb.com>
2020-09-24 17:04:02 -0700

    UPSTREAM: bpf: Change bpf_sk_storage_*() to accept ARG_PTR_TO_BTF_ID_SOCK_COMMON
    This patch changes the bpf_sk_storage_*() to take
    ARG_PTR_TO_BTF_ID_SOCK_COMMON such that they will work with the pointer
    returned by the bpf_skc_to_*() helpers also.
    
    A micro benchmark has been done on a "cgroup_skb/egress" bpf program
    which does a bpf_sk_storage_get().  It was driven by netperf doing
    a 4096 connected UDP_STREAM test with 64bytes packet.
    The stats from "kernel.bpf_stats_enabled" shows no meaningful difference.
    
    The sk_storage_get_btf_proto, sk_storage_delete_btf_proto,
    btf_sk_storage_get_proto, and btf_sk_storage_delete_proto are
    no longer needed, so they are removed.
    
    Change-Id: I4547813f0fe3b60b277c508d02acc6d783a309ed
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Lorenz Bauer <lmb@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20200925000402.3856307-1-kafai@fb.com

--- net/core/bpf_sk_storage.c
+++ net/core/bpf_sk_storage.c
@@ -374,42 +374,12 @@ const struct bpf_func_proto bpf_sk_storage_get_cg_sock_proto = {
 	.arg4_type	= ARG_ANYTHING,
 };
 
-const struct bpf_func_proto bpf_sk_storage_get_cg_sock_proto = {
-	.func		= bpf_sk_storage_get,
-	.gpl_only	= false,
-	.ret_type	= RET_PTR_TO_MAP_VALUE_OR_NULL,
-	.arg1_type	= ARG_CONST_MAP_PTR,
-	.arg2_type	= ARG_PTR_TO_CTX, /* context is 'struct sock' */
-	.arg3_type	= ARG_PTR_TO_MAP_VALUE_OR_NULL,
-	.arg4_type	= ARG_ANYTHING,
-};
-
 const struct bpf_func_proto bpf_sk_storage_delete_proto = {
 	.func		= bpf_sk_storage_delete,
 	.gpl_only	= false,
 	.ret_type	= RET_INTEGER,
 	.arg1_type	= ARG_CONST_MAP_PTR,
 	.arg2_type	= ARG_PTR_TO_BTF_ID_SOCK_COMMON,
-};
-
-const struct bpf_func_proto sk_storage_get_btf_proto = {
-	.func		= bpf_sk_storage_get,
-	.gpl_only	= false,
-	.ret_type	= RET_PTR_TO_MAP_VALUE_OR_NULL,
-	.arg1_type	= ARG_CONST_MAP_PTR,
-	.arg2_type	= ARG_PTR_TO_BTF_ID,
-	.arg2_btf_id	= &btf_sock_ids[BTF_SOCK_TYPE_SOCK],
-	.arg3_type	= ARG_PTR_TO_MAP_VALUE_OR_NULL,
-	.arg4_type	= ARG_ANYTHING,
-};
-
-const struct bpf_func_proto sk_storage_delete_btf_proto = {
-	.func		= bpf_sk_storage_delete,
-	.gpl_only	= false,
-	.ret_type	= RET_INTEGER,
-	.arg1_type	= ARG_CONST_MAP_PTR,
-	.arg2_type	= ARG_PTR_TO_BTF_ID,
-	.arg2_btf_id	= &btf_sock_ids[BTF_SOCK_TYPE_SOCK],
 };
 
 struct bpf_sk_storage_diag {
