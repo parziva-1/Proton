56426278061a8926e55ab1eeacfadd01fa61408b
Song Liu <songliubraving@fb.com>
2020-09-30 15:49:26 -0700

    UPSTREAM: bpf: Introduce BPF_F_PRESERVE_ELEMS for perf event array
    Currently, perf event in perf event array is removed from the array when
    the map fd used to add the event is closed. This behavior makes it
    difficult to the share perf events with perf event array.
    
    Introduce perf event map that keeps the perf event open with a new flag
    BPF_F_PRESERVE_ELEMS. With this flag set, perf events in the array are not
    removed when the original map fd is closed. Instead, the perf event will
    stay in the map until 1) it is explicitly removed from the array; or 2)
    the array is freed.
    
    Change-Id: I56f89d58a60a0d7a2d377916f0e80c99abc0a73c
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200930224927.1936644-2-songliubraving@fb.com

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -15,7 +15,8 @@
 #include "map_in_map.h"
 
 #define ARRAY_CREATE_FLAG_MASK \
-	(BPF_F_NUMA_NODE | BPF_F_MMAPABLE | BPF_F_ACCESS_MASK)
+	(BPF_F_NUMA_NODE | BPF_F_MMAPABLE | BPF_F_ACCESS_MASK | \
+	 BPF_F_PRESERVE_ELEMS)
 
 static void bpf_array_free_percpu(struct bpf_array *array)
 {
@@ -62,6 +63,10 @@ int array_map_alloc_check(union bpf_attr *attr)
 
 	if (attr->map_type != BPF_MAP_TYPE_ARRAY &&
 	    attr->map_flags & BPF_F_MMAPABLE)
+		return -EINVAL;
+
+	if (attr->map_type != BPF_MAP_TYPE_PERF_EVENT_ARRAY &&
+	    attr->map_flags & BPF_F_PRESERVE_ELEMS)
 		return -EINVAL;
 
 	if (attr->value_size > KMALLOC_MAX_SIZE)
@@ -1150,6 +1155,13 @@ static void perf_event_fd_array_release(struct bpf_map *map,
 			fd_array_map_delete_elem(map, &i);
 	}
 	rcu_read_unlock();
+}
+
+static void perf_event_fd_array_map_free(struct bpf_map *map)
+{
+	if (map->map_flags & BPF_F_PRESERVE_ELEMS)
+		bpf_fd_array_map_clear(map);
+	fd_array_map_free(map);
 }
 
 static int perf_event_array_map_btf_id;
