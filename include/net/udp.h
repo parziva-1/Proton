647c935f99c385c0334f16bc48852cad938965d2
Cong Wang <cong.wang@bytedance.com>
2021-02-23 10:49:26 -0800

    BACKPORT: bpf: Clean up sockmap related Kconfigs
    [ Upstream commit 887596095ec2a9ea39ffcf98f27bf2e77c5eb512 ]
    
    As suggested by John, clean up sockmap related Kconfigs:
    
    Reduce the scope of CONFIG_BPF_STREAM_PARSER down to TCP stream
    parser, to reflect its name.
    
    Make the rest sockmap code simply depend on CONFIG_BPF_SYSCALL
    and CONFIG_INET, the latter is still needed at this point because
    of TCP/UDP proto update. And leave CONFIG_NET_SOCK_MSG untouched,
    as it is used by non-sockmap cases.
    
    Change-Id: I92d53e4edb7c56a0b60a8c1eefb50fb16db4533f
    Signed-off-by: Cong Wang <cong.wang@bytedance.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Reviewed-by: Lorenz Bauer <lmb@cloudflare.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Jakub Sitnicki <jakub@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20210223184934.6054-2-xiyou.wangcong@gmail.com
    Stable-dep-of: 2660a544fdc0 ("net: Fix TOCTOU issue in sk_is_readable()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/net/udp.h
+++ include/net/udp.h
@@ -519,9 +519,9 @@ drop:
 	return NULL;
 }
 
-#ifdef CONFIG_BPF_STREAM_PARSER
+#ifdef CONFIG_BPF_SYSCALL
 struct sk_psock;
 struct proto *udp_bpf_get_proto(struct sock *sk, struct sk_psock *psock);
-#endif /* BPF_STREAM_PARSER */
+#endif
 
 #endif	/* _UDP_H */
