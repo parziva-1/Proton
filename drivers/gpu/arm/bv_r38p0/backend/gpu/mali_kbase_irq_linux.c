21695ed8c9c49135d06287b32e388e02b8780d67
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 00:00:32 -0700

    mm: Perform PID map reads on the little CPU cluster
    PID map reads for processes with thousands of mappings can be done
    extensively by certain Android apps, burning through CPU time on
    higher-performance CPUs even though reading PID maps is never a
    performance-critical task. We can relieve the load on the important CPUs
    by moving PID map reads to little CPUs via sched_migrate_to_cpumask_*().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/arm/bv_r38p0/backend/gpu/mali_kbase_irq_linux.c
+++ drivers/gpu/arm/bv_r38p0/backend/gpu/mali_kbase_irq_linux.c
@@ -452,10 +452,22 @@ int kbase_install_interrupts(struct kbase_device *kbdev)
 	u32 nr = ARRAY_SIZE(kbase_handler_table);
 	int err;
 	u32 i;
-
+	unsigned long flags;
+	
 	for (i = 0; i < nr; i++) {
+		flags = kbdev->irqs[i].flags | IRQF_SHARED;
+
+
+
+
+
+		if (i == JOB_IRQ_TAG)
+
+
+			flags |= IRQF_PERF_AFFINE; /* Apply perf flag only to JOB IRQ */
+			
 		err = request_irq(kbdev->irqs[i].irq, kbase_handler_table[i],
-				kbdev->irqs[i].flags | IRQF_SHARED,
+				flags,
 				dev_name(kbdev->dev),
 				kbase_tag(kbdev, i));
 		if (err) {
