365763c458598e2d71d2802020933bf0ebebf3f4
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-21 04:19:07 -0300

    fs: susfs: default to sus_su mode 2
    This is equivalent to using manual hooks.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- fs/susfs.c
+++ fs/susfs.c
@@ -844,7 +844,7 @@ struct filename* susfs_get_redirected_path(unsigned long ino) {
 /* sus_su */
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 bool susfs_is_sus_su_hooks_enabled __read_mostly = false;
-static int susfs_sus_su_working_mode = 0;
+static int susfs_sus_su_working_mode = 2;
 extern void ksu_susfs_enable_sus_su(void);
 extern void ksu_susfs_disable_sus_su(void);
 
