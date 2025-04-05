e991eee994cb7a0e40fb09414030523124663408
elitetest1 <tizzyprove@gmail.com>
2025-04-05 04:31:27 -0300

    schedutil tweaks

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -876,8 +876,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 1000;
+	tunables->up_rate_limit_us = 200;
+	tunables->down_rate_limit_us = 800;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
