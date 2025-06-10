2225dfa005af73c0a39c30900930aa708c6963d4
Sergey Senozhatsky <senozhatsky@chromium.org>
2025-06-10 13:52:29 +0600

    fuse: use freezable wait in fuse_get_req()
    Use freezable wait in fuse_get_req() so that it won't block
    the system from entering suspend:
    
     Freezing user space processes failed after 20.009 seconds
     Call trace:
      __switch_to+0xcc/0x168
      schedule+0x57c/0x1138
      fuse_get_req+0xd0/0x2b0
      fuse_simple_request+0x120/0x620
      fuse_getxattr+0xe4/0x158
      fuse_xattr_get+0x2c/0x48
      __vfs_getxattr+0x160/0x1d8
      get_vfs_caps_from_disk+0x74/0x1a8
      __audit_inode+0x244/0x4d8
      user_path_at_empty+0x2e0/0x390
      __arm64_sys_faccessat+0xdc/0x260
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -207,9 +207,9 @@ static struct fuse_req *fuse_get_req(struct fuse_conn *fc, bool for_background)
 
 	if (fuse_block_alloc(fc, for_background)) {
 		err = -EINTR;
-		/* @fs.sec -- 9992f9e9ebd25b0dcc80951a9e4f4fc2e71a08c6 -- */
-		if (fuse_wait_event_killable_exclusive(fc->blocked_waitq,
-				!fuse_block_alloc(fc, for_background)))
+		if (wait_event_state_exclusive(fc->blocked_waitq,
+				!fuse_block_alloc(fc, for_background),
+				(TASK_KILLABLE | TASK_FREEZABLE)))
 			goto out;
 	}
 	/* Matches smp_wmb() in fuse_set_initialized() */
