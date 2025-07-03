de55c1faa600597227f293c426b501b5f3f8bdda
EliteGalaxy <tizzyprove@gmail.com>
2025-07-02 21:06:30 -0300

    Revert "drivers: sec_battery: fixed compilation issues."
    This reverts commit 094c1d71b5b294c8bb4b71ade114ae6a932418e5.

--- drivers/battery/common/sec_battery.c
+++ drivers/battery/common/sec_battery.c
@@ -1284,9 +1284,9 @@ void sec_bat_set_charging_status(struct sec_battery_info *battery, int status)
 		break;
 	case POWER_SUPPLY_STATUS_NOT_CHARGING:
 	case POWER_SUPPLY_STATUS_DISCHARGING:
-		if ((battery->status == POWER_SUPPLY_STATUS_FULL ||
+		if (battery->status == POWER_SUPPLY_STATUS_FULL ||
 			(battery->capacity == 100 && !is_slate_mode(battery))) &&
-			!battery->store_mode && battery->charging_enabled) {
+			!battery->store_mode && battery->charging_enabled && !is_eu_eco_rechg(battery->fs) {
 
 			pr_info("%s : Update fg scale to 101%%\n", __func__);
 			value.intval = 100;
