7e8617d29e42d5edcb696d5dc5079f8ff5038373
EliteGalaxy <tizzyprove@gmail.com>
2025-07-02 21:06:40 -0300

    Revert "battery: sec_battery: Implement battery charging_enabled node"
    This reverts commit 699f8ec3b7b7d813a51c5fe1119623402b7f11c8.

--- drivers/battery/common/sec_battery.h
+++ drivers/battery/common/sec_battery.h
@@ -1141,10 +1141,6 @@ struct sec_battery_info {
 	bool display_test;
 	bool store_mode;
 
-	/* charging disabler */
-	bool charging_enabled;
-	bool charging_suspended;
-
 	/* usb suspend */
 	int prev_usb_conf;
 
