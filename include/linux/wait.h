ab741de42006ce4df0765bdb7bc020171bc0a787
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-08-29 00:56:32 +0600

    {fuse, sched/wait}: Replicate pre-TASK_FREEZABLE behavior on downstream
    msm-5.4 lacks TASK_FREEZABLE which was introduced with freezer rewrite in
    upstream [1]. That's a huge backport that's not worth for the small change in
    fuse. Instead, replicate how the logic worked before the introduction of
    TASK_FREEZABLE in upstream and adapt for our kernel.
    
    Now wait_event_state_exclusive() is only exclusive for freezable tasks. So,
    let's edit the macro name to wait_event_freezable_state_exclusive().
    
    [1] https://github.com/torvalds/linux/commit/f5d39b020809146
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/wait.h
+++ include/linux/wait.h
@@ -645,6 +645,18 @@ do {										\
 	__ret;									\
 })
 
+#define __wait_event_freezable_state_exclusive(wq, condition, state)			\
+	___wait_event(wq, condition, state, 1, 0, freezable_schedule())
+
+#define wait_event_freezable_state_exclusive(wq, condition, state)			\
+({											\
+	int __ret = 0;									\
+	might_sleep();									\
+	if (!(condition))								\
+		__ret = __wait_event_freezable_state_exclusive(wq, condition, state);	\
+	__ret;										\
+})
+
 /**
  * wait_event_idle - wait for a condition without contributing to system load
  * @wq_head: the waitqueue to wait on
