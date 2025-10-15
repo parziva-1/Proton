81055de54287f37146ec01e2839bfda8e6b7f672
elitetest1 <tizzyprove@gmail.com>
2025-10-15 13:58:50 -0300

    Revert "fs: fsync on/off support"
    This reverts commit 8338ad8ae007db8c2f4602816aac1ab334e57d36.

--- fs/sync.c
+++ fs/sync.c
@@ -169,9 +169,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	int ret;
 
 	if (!fsync_enabled)
-	return 0;
+		return 0;
 
-f	 = fdget(fd);
+	f = fdget(fd);
 
 	if (!f.file)
 		return -EBADF;
@@ -201,11 +201,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	struct inode *inode = file->f_mapping->host;
 
 	if (!fsync_enabled)
-<<<<<<< HEAD
 		return 0;
-=======
-	return 0;
->>>>>>> 284198e8ffd6 (fs: fsync on/off support)
 
 	if (!file->f_op->fsync)
 		return -EINVAL;
@@ -238,11 +234,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	int ret = -EBADF;
 
 	if (!fsync_enabled)
-<<<<<<< HEAD
 		return 0;
-=======
-	return 0;
->>>>>>> 284198e8ffd6 (fs: fsync on/off support)
 
 	f = fdget(fd);
 
@@ -257,11 +249,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 	if (!fsync_enabled)
-<<<<<<< HEAD
 		return 0;
-=======
-	return 0;
->>>>>>> 284198e8ffd6 (fs: fsync on/off support)
 
 	return do_fsync(fd, 0);
 }
@@ -269,11 +257,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 	if (!fsync_enabled)
-<<<<<<< HEAD
 		return 0;
-=======
-	return 0;
->>>>>>> 284198e8ffd6 (fs: fsync on/off support)
 
 	return do_fsync(fd, 1);
 }
@@ -287,11 +271,7 @@ int sync_file_range(struct file *file, loff_t offset, loff_t nbytes,
 	umode_t i_mode;
 
 	if (!fsync_enabled)
-<<<<<<< HEAD
 		return 0;
-=======
-	return 0;
->>>>>>> 284198e8ffd6 (fs: fsync on/off support)
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
