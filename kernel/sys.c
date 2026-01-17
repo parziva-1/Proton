parziva-1/Proton

commit a90302e5e07a57eacdebe21b9d6e1df4f7e56fd8

syscall: Only spoof uname for root
* This fixes root/custom ROM detection in apps

Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

kernel/sys.c
@@ -1263,23 +1263,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifndef CONFIG_FAKE_UNAME_NONE
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4) ||
-	    !strncmp(current->comm, "uprobestats", 11)) {
-#if defined(CONFIG_FAKE_UNAME_5_4)
-		strcpy(tmp.release, "5.4.200");
-#elif defined(CONFIG_FAKE_UNAME_5_10)
-		strcpy(tmp.release, "5.10.200");
-#elif defined(CONFIG_FAKE_UNAME_5_15)
-		strcpy(tmp.release, "5.15.200");
-#elif defined(CONFIG_FAKE_UNAME_6_1)
-		strcpy(tmp.release, "6.1.200");
-#elif defined(CONFIG_FAKE_UNAME_6_6)
-		strcpy(tmp.release, "6.6.200");
-#elif defined(CONFIG_FAKE_UNAME_6_12)
-		strcpy(tmp.release, "6.12.200");
-#endif
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+	    !strncmp(current->comm, "netd", 4)) {
+		if (current_uid().val == 0) {
+			strcpy(tmp.release, "5.10.200");
+			pr_debug("fake uname: %s/%d release=%s\n",
+				 current->comm, current->pid, tmp.release);
+		}
 	}
 #endif
 	up_read(&uts_sem);

commit b15596f37210395db423f0376a46bc92af3cf2b9

syscall: Fix derp caused by a90302e5e07a57eacdebe21b9d6e1df4f7e56fd8
This commit was already wrong from where it was picked from. It removed
uprobestats from the spoofing list and broke the spoof selection.

Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

kernel/sys.c
@@ -1263,9 +1263,22 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #ifndef CONFIG_FAKE_UNAME_NONE
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4)
+		!strncmp(current->comm, "uprobestats", 11)) {
 		if (current_uid().val == 0) {
+#if defined(CONFIG_FAKE_UNAME_5_4)
+			strcpy(tmp.release, "5.4.200");
+#elif defined(CONFIG_FAKE_UNAME_5_10)
 			strcpy(tmp.release, "5.10.200");
+#elif defined(CONFIG_FAKE_UNAME_5_15)
+			strcpy(tmp.release, "5.15.200");
+#elif defined(CONFIG_FAKE_UNAME_6_1)
+			strcpy(tmp.release, "6.1.200");
+#elif defined(CONFIG_FAKE_UNAME_6_6)
+			strcpy(tmp.release, "6.6.200");
+#elif defined(CONFIG_FAKE_UNAME_6_12)
+			strcpy(tmp.release, "6.12.200");
+#endif
 			pr_debug("fake uname: %s/%d release=%s\n",
 				 current->comm, current->pid, tmp.release);
 		}