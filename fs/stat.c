dfa9e6117bd10fe664e62f354ed4f56331e38600
Flopster101 <nahuelgomez329@gmail.com>
2026-03-19 01:58:07 -0300

    fs: stat: Guard off some KSU stat hooks
    KSU Next doesn't use these, but I don't wanna remove them yet in case it uses
    them in the future.
    
    ld.lld: error: undefined symbol: ksu_handle_newfstat_ret
    >>> referenced by stat.c:418 (../fs/stat.c:418)
    >>>               vmlinux.o:(__arm64_sys_newfstat)
    
    ld.lld: error: undefined symbol: ksu_handle_fstat64_ret
    >>> referenced by stat.c:549 (../fs/stat.c:549)
    >>>               vmlinux.o:(__arm64_sys_fstat64)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -381,11 +381,13 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 
 #ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#if 0
 extern void ksu_handle_newfstat_ret(unsigned int *fd,
 			struct stat __user **statbuf_ptr);
 #if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
 extern void ksu_handle_fstat64_ret(unsigned long *fd,
 			struct stat64 __user **statbuf_ptr);
+#endif
 #endif
 #endif
 
@@ -414,8 +416,10 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
 #ifdef CONFIG_KSU_MANUAL_HOOK
+#if 0
 	if (!error)
 		ksu_handle_newfstat_ret(&fd, &statbuf);
+#endif
 #endif
 
 	return error;
@@ -545,8 +549,10 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
 #ifdef CONFIG_KSU_MANUAL_HOOK
+#if 0
 	if (!error)
 		ksu_handle_fstat64_ret(&fd, &statbuf);
+#endif
 #endif
 
 	return error;
