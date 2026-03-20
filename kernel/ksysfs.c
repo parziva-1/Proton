e126c9449b8f13ac2fede3c5286fc66b32158698
Flopster101 <nahuelgomez329@gmail.com>
2026-03-20 17:15:26 -0300

    kernel: Add sysfs knob to turn on/off the throttling blocker
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/ksysfs.c
+++ kernel/ksysfs.c
@@ -11,6 +11,7 @@
 #include <linux/sysfs.h>
 #include <linux/export.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/kexec.h>
 #include <linux/profile.h>
 #include <linux/stat.h>
@@ -182,6 +183,37 @@ static ssize_t rcu_normal_store(struct kobject *kobj,
 KERNEL_ATTR_RW(rcu_normal);
 #endif /* #ifndef CONFIG_TINY_RCU */
 
+static bool freq_control_blocking = true;
+
+bool freq_control_blocking_enabled(void)
+{
+	return READ_ONCE(freq_control_blocking);
+}
+
+static ssize_t freq_control_blocking_enabled_show(struct kobject *kobj,
+						  struct kobj_attribute *attr,
+						  char *buf)
+{
+	return sprintf(buf, "%d\n", freq_control_blocking_enabled());
+}
+
+static ssize_t freq_control_blocking_enabled_store(struct kobject *kobj,
+						   struct kobj_attribute *attr,
+						   const char *buf,
+						   size_t count)
+{
+	bool enable;
+
+	if (kstrtobool(buf, &enable))
+		return -EINVAL;
+
+	WRITE_ONCE(freq_control_blocking, enable);
+	pr_info("freq control blocking %s\n", enable ? "enabled" : "disabled");
+
+	return count;
+}
+KERNEL_ATTR_RW(freq_control_blocking_enabled);
+
 /*
  * Make /sys/kernel/notes give the raw contents of our kernel .notes section.
  */
@@ -226,10 +258,11 @@ static struct attribute * kernel_attrs[] = {
 	&vmcoreinfo_attr.attr,
 #endif
 #ifndef CONFIG_TINY_RCU
-	&rcu_expedited_attr.attr,
-	&rcu_normal_attr.attr,
+		&rcu_expedited_attr.attr,
+		&rcu_normal_attr.attr,
 #endif
-	NULL
+		&freq_control_blocking_enabled_attr.attr,
+		NULL
 };
 
 static const struct attribute_group kernel_attr_group = {
