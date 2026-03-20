d97992ca6da244b518a5a1e435722f4f310291d1
Flopster101 <nahuelgomez329@gmail.com>
2026-03-20 17:11:41 -0300

    treewide: Block userspace daemons from throttling the CPU
    A new function called task_controls_frequencies() that works like task_is_booster()
    is added to check if the task trying to modify the max frequency is one that should
    or not be allowed.
    
    The kernel already does throttling, so these are pointless.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -170,4 +170,20 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strncmp(comm, "system_perf_ini", 9);
 }
 
+static inline bool task_controls_frequencies(struct task_struct *tsk)
+{
+	char comm[sizeof(tsk->comm)];
+
+	if (task_is_booster(tsk))
+		return true;
+
+	get_task_comm(comm, tsk);
+	return !strncmp(comm, "thermal_", 8) ||
+	       !strcmp(comm, "HyPerThread") ||
+	       !strcmp(comm, "argosd") ||
+	       !strncmp(comm, "POSIX timer", 11) ||
+	       !strcmp(comm, "RenderEngine") ||
+	       !strcmp(comm, "mali-cmar-backe");
+}
+
 #endif /* _LINUX_BINFMTS_H */
